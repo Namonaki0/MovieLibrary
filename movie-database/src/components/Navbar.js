7177856c392399aff62cd00b9b2b91be30f97d7f
Andre <andreferreira@live.co.uk>
2021-08-12 23:11:19 +0100

    first commit

---
+++ movie-database/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav>
+      <a href="#">Index</a>
+      <a href="#">Upcoming</a>
+      <a href="#">Classics</a>
+    </nav>
+  );
+}
+
+export default Navbar;
