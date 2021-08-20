21ace11c73d687671fb0d108608433e9fb0572ee
Andre <andreferreira@live.co.uk>
2021-08-20 23:08:02 +0100

    api movie image fetching

--- movie-database/src/components/main/Library.js
+++ movie-database/src/components/main/Library.js
@@ -8,16 +8,30 @@ export default function Library() {
 
   const [movies, setMovies] = useState([]);
 
+  const api_key = `fef4f456a619d2054596d72fcd9a7171`;
+
   const apiFetch = async () => {
     const apiCall = await fetch(
-      "https://api.themoviedb.org/3/search/company?api_key=fef4f456a619d2054596d72fcd9a7171&query=zeitgeist&page=1"
+      `https://api.themoviedb.org/3/search/company?api_key=${api_key}&query=zeitgeist&page=1`
     );
     const movies = await apiCall.json();
+
     setMovies(movies.results);
   };
+
   movies.map((movie) => {
-    console.log(movie);
+    const movie_id = movie.id;
+    console.log(movie_id);
   });
+
+  const fetch_images = async (movie_id) => {
+    const images = await fetch(
+      `https://api.themoviedb.org/3/movie/${movie_id}/images?api_key=${api_key}&language=en-US&include_image_language=en,null`
+    );
+
+    const imagesData = await images.json();
+    console.log(imagesData);
+  };
 
   return (
     <div className="movie-template">
