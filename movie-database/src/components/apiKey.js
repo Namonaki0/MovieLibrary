d7fb60e25fc0807e611c1ebd08f97ca8d0b8ec34
Andre <andreferreira@live.co.uk>
2021-08-31 23:08:52 +0100

    upcoming movie section

---
+++ movie-database/src/components/apiKey.js
@@ -0,0 +1,1 @@
+export const api_key = `fef4f456a619d2054596d72fcd9a7171`;
--- movie-database/src/components/main/Upcoming.js
+++ movie-database/src/components/main/Upcoming.js
@@ -1,5 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { api_key } from "../apiKey";
 
 export default function Upcoming() {
-  return <main>UPCOMING</main>;
+  const [upcomingMovies, setUpcomingMovies] = useState([]);
+
+  useEffect(async () => {
+    const url = `https://api.themoviedb.org/3/movie/upcoming?api_key=${api_key}&language=en-US&page=1`;
+
+    try {
+      const upcomingApiFetch = await fetch(url);
+      const upcomingMovies = await upcomingApiFetch.json();
+      // setUpcomingMovies(upcomingMovies.results);
+      // console.log(upcomingMovies.results);
+    } catch (err) {
+      console.error(err);
+    }
+  });
+
+  return (
+    <div>
+      <h1>Upcoming Movies</h1>
+      <div className="upcoming-movie-wrapper">
+        {upcomingMovies.map((upcomingMovie) => (
+          <h2>{upcomingMovie.original_title}</h2>
+        ))}
+      </div>
+    </div>
+  );
 }
