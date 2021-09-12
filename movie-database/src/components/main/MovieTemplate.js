aba3a7bd0bd8a59c95d00fbd3403ccca14afd9b8
Andre <andreferreira@live.co.uk>
2021-09-12 23:11:26 +0100

    json server added

--- movie-database/src/components/main/MovieTemplate.js
+++ movie-database/src/components/main/MovieTemplate.js
@@ -1,26 +1,37 @@
 import React, { useState, useEffect } from "react";
-import Modal from "react-modal";
+// import Modal from "react-modal";
 
-const MovieTemplate = (movie) => {
+const MovieTemplate = ({
+  movie,
+  title,
+  src,
+  overview,
+  release_date,
+  vote_average,
+}) => {
+  // const [openModal, setOpenModal] = useState(false);
   return (
     <div>
       <div
         className="movie-container"
         key={movie.id}
-        onClick={() => setOpenModal(true)}
+        // onClick={() => setOpenModal(true)}
       >
-        <img
-          className="movie-image"
-          src={movie.src}
-          alt={movie.title + "poster"}
-        />
-        <h1>{movie.title}</h1>
-        <div className="movie-overview">{movie.overview}</div>
-        <div className="movie-release-date">
-          Release date: {movie.release_date}
-        </div>
-        <div className="movie-rating">Rating: {movie.vote_average}</div>
+        <img className="movie-image" src={src} alt={title + "poster"} />
+        <h1>{title}</h1>
+        <div className="movie-overview">{overview}</div>
+        <div className="movie-release-date">Release date: {release_date}</div>
+        <div className="movie-rating">Rating: {vote_average}</div>
       </div>
+
+      {/* <Modal
+        isOpen={openModal}
+        shouldCloseOnOverlayClick={false}
+        onRequestClose={() => setOpenModal(false)}
+        className="new-modal"
+      >
+        <h1>{movie.title}</h1>
+      </Modal> */}
     </div>
   );
 };
