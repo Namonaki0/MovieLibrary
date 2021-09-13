057ea1923089721421067deb9c598dec8898eec8
Andre <andreferreira@live.co.uk>
2021-09-13 13:44:12 +0100

    changes to current modal and CSS

--- movie-database/src/components/main/Favorites.js
+++ movie-database/src/components/main/Favorites.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function Favorites(props) {
+export default function Favorites() {
   return (
     <div className="favorites-wrapper">
       <h1>Favorites</h1>
--- movie-database/src/components/main/MovieTemplate.js
+++ movie-database/src/components/main/MovieTemplate.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from "react";
-// import Modal from "react-modal";
+import React from "react";
 
 const MovieTemplate = ({
   movie,
@@ -9,29 +8,13 @@ const MovieTemplate = ({
   release_date,
   vote_average,
 }) => {
-  // const [openModal, setOpenModal] = useState(false);
   return (
-    <div>
-      <div
-        className="movie-container"
-        key={movie.id}
-        // onClick={() => setOpenModal(true)}
-      >
-        <img className="movie-image" src={src} alt={title + "poster"} />
-        <h1>{title}</h1>
-        <div className="movie-overview">{overview}</div>
-        <div className="movie-release-date">Release date: {release_date}</div>
-        <div className="movie-rating">Rating: {vote_average}</div>
-      </div>
-
-      {/* <Modal
-        isOpen={openModal}
-        shouldCloseOnOverlayClick={false}
-        onRequestClose={() => setOpenModal(false)}
-        className="new-modal"
-      >
-        <h1>{movie.title}</h1>
-      </Modal> */}
+    <div className="movie-container" key={movie.id}>
+      <img className="movie-image" src={src} alt={title + "poster"} />
+      <h1>{title}</h1>
+      <div className="movie-overview">{overview}</div>
+      <div className="movie-release-date">Release date: {release_date}</div>
+      <div className="movie-rating">Rating: {vote_average}</div>
     </div>
   );
 };
