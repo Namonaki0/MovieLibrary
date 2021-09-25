2257aad5c5df99f566e22f9f5c9e58e40efa7932
Andre <andreferreira@live.co.uk>
2021-09-25 22:52:19 +0100

    watchlist micro interactions

--- movie-database/src/components/main/WatchlistMovieTemplate.js
+++ movie-database/src/components/main/WatchlistMovieTemplate.js
@@ -1,14 +1,18 @@
 const WatchlistMovieTemplate = ({ listedMovie }) => {
   return (
     <div className="watchlist-movie-wrapper">
-      <div>
+      <div className="watchlist-info-slider">
         <h2 className="watchlist-movie">{listedMovie.title}</h2>
-        <img
-          className="watchlist-movie-image"
-          src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${listedMovie.poster_path}`}
-          alt={listedMovie.title + "poster"}
-        />
+        <p>RATING: {listedMovie.vote_average}</p>
+        <button className="remove-from-watchlist-btn">
+          remove from watchlist
+        </button>
       </div>
+      <img
+        className="watchlist-movie-image"
+        src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${listedMovie.poster_path}`}
+        alt={listedMovie.title + "poster"}
+      />
     </div>
   );
 };
